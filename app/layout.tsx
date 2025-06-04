2a1e0b9fda8e46e7b217e2f78d51f173ed673b6e
Deeksha Rungta <deeksharu1902@gmail.com>
2025-06-05 01:56:05 +0400

    feat: implement viewport settings and dynamic metadata generation in layout for enhanced responsiveness and SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Viewport } from "next";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -9,11 +10,40 @@ const outfit = Outfit({
   weight: ["300", "400", "500", "600"],
 });
 
-export const metadata: Metadata = {
-  title: "XCast",
-  description: "Cross-post your tweets to Farcaster",
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
+export async function generateMetadata(): Promise<Metadata> {
+  const URL = process.env.NEXT_PUBLIC_URL;
+  return {
+    title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+    description: "Cross-post your tweets to Farcaster",
+    openGraph: {
+      title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+      description: "Cross-post your tweets to Farcaster",
+      images: [process.env.NEXT_PUBLIC_APP_HERO_IMAGE || ""],
+    },
+    other: {
+      "fc:frame": JSON.stringify({
+        version: "next",
+        imageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
+        button: {
+          title: `Cross-post your tweets to Farcaster`,
+          action: {
+            type: "launch_frame",
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+            url: URL,
+            splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE,
+            splashBackgroundColor:
+              process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
+          },
+        },
+      }),
+    },
+  };
+}
 export default function RootLayout({
   children,
 }: {
