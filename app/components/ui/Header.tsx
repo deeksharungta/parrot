b4c5d9eed2a06ec0e9756dc89341def6e55cd664
Deeksha Rungta <deeksharu1902@gmail.com>
2025-06-23 14:16:54 +0400

    feat: add Early Access button and modal to Header component for improved user engagement

--- app/components/ui/Header.tsx
+++ app/components/ui/Header.tsx
@@ -2,31 +2,49 @@
 
 import { useMiniKit } from "@coinbase/onchainkit/minikit";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
+import EarlyAccessModal from "../promotional/EarlyAccessModal";
+import Settings from "../icons/Settings";
 
 export default function Header({ title }: { title: string }) {
   const { context } = useMiniKit();
+  const [earlyAccessModalOpen, setEarlyAccessModalOpen] = useState(false);
 
   return (
-    <div className="flex items-center justify-between sticky top-0 bg-white z-10 p-5">
-      <motion.p
-        className="font-medium text-2xl text-[#494656]"
-        initial={{ opacity: 0, x: -20 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.4, delay: 0.1 }}
-      >
-        {title}
-      </motion.p>
-      <div className="rounded-full overflow-hidden">
-        <Image
-          src={context?.user?.pfpUrl ?? "/farcaster.png"}
-          alt="profile"
-          width={30}
-          height={30}
-          className="rounded-full"
-        />
+    <>
+      <div className="flex items-center justify-between sticky top-0 bg-white z-10 p-5">
+        <motion.p
+          className="font-medium text-2xl text-[#494656]"
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.4, delay: 0.1 }}
+        >
+          {title}
+        </motion.p>
+        <div className="flex items-center gap-2">
+          <button
+            onClick={() => {
+              setEarlyAccessModalOpen(true);
+            }}
+          >
+            <Settings isActive={false} />
+          </button>
+          <div className="rounded-full overflow-hidden">
+            <Image
+              src={context?.user?.pfpUrl ?? "/farcaster.png"}
+              alt="profile"
+              width={30}
+              height={30}
+              className="rounded-full"
+            />
+          </div>
+        </div>
       </div>
-    </div>
+      <EarlyAccessModal
+        isOpen={earlyAccessModalOpen}
+        onClose={() => setEarlyAccessModalOpen(false)}
+      />
+    </>
   );
 }
